a9d197cc90ec773b461f0353145cc3b9d4f0ec7b
James <james@team2168.org>
2018-01-13 16:57:24 -0500

    Initial commit
    Copy in 2017 baseline - updated for 2018 WPILib changes.

---
+++ src/org/team2168/Robot.java
@@ -0,0 +1,348 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package org.team2168;
+
+
+import org.team2168.subsystems.*;
+import org.team2168.subsystems.drivetrain.*;
+import org.team2168.commands.auto.*;
+import org.team2168.commands.pneumatics.*;
+import org.team2168.utils.Debouncer;
+import org.team2168.utils.PowerDistribution;
+import org.team2168.utils.TX1TurnON;
+import org.team2168.utils.consoleprinter.ConsolePrinter;
+
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.Scheduler;
+import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+/**
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the IterativeRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the build.properties file in the
+ * project.
+ */
+public class Robot extends TimedRobot {
+	private static DigitalInput practiceBot;
+
+	//Subsystems instance variables
+	//  If you need to access a subsystem from anywhere in the code,
+	//  it's done through one of these variables.
+	public static Drivetrain drivetrain;
+	public static Pneumatics pneumatics;
+
+	public static PowerDistribution pdp;
+	public static Compressor comp;
+    public static TX1TurnON tx1;
+    public static DrivetrainIMUGlobalPosition dtIMU;
+
+	//Variables for initializing and calibrating the Gyro
+    private static boolean matchStarted = false;
+    public static int gyroReinits;
+	private double lastAngle;
+	private Debouncer gyroDriftDetector = new Debouncer(1.0);
+	public static boolean gyroCalibrating = false;
+	private boolean lastGyroCalibrating = false;
+	private double curAngle = 0.0;
+
+	public static OI oi;
+	
+    public static DriverStation driverstation;
+	private static final String kDefaultAuto = "Default";
+	private static final String kCustomAuto = "My Auto";
+	private String m_autoSelected;
+	private SendableChooser<String> m_chooser = new SendableChooser<>();
+	private static boolean autoMode;
+	static Command autonomousCommand;
+    public static SendableChooser<Command> autoChooser;
+    
+    private static int controlStyle;
+    public static SendableChooser<Number> controlStyleChooser;
+    
+	private static boolean blueAlliance = false;
+	
+	/**
+	 * This function is run when the robot is first started up and should be
+	 * used for any initialization code.
+	 */
+	@Override
+	public void robotInit() {
+		this.setPeriod(RobotMap.MAIN_PERIOD_S);
+		
+		ConsolePrinter.init();
+    	ConsolePrinter.setRate(RobotMap.CONSOLE_PRINTER_LOG_RATE_MS);
+    	
+    	practiceBot = new DigitalInput(RobotMap.PRACTICE_BOT_JUMPER);
+
+    	// Instantiate the subsystems
+    	drivetrain = Drivetrain.getInstance();
+    	pneumatics = Pneumatics.getInstance();
+    	
+//    	drivetrainShifter = DrivetrainShifter.getInstance();
+//    	flashlight = Flashlight.getInstance();
+//    	gearIntakeArm = GearIntakeArm.getInstance();
+//    	gearIntakeRoller = GearIntakeRoller.getInstance();
+//    	shooterHood = ShooterHood.getInstance();
+//    	shooterIndexer = ShooterIndexer.getInstance();
+//    	shooterWheel = ShooterWheel.getInstance();
+//    	turret = Turret.getInstance();
+    	
+
+    	
+        oi = OI.getInstance();
+        
+        //run compressor
+        new StartCompressor();
+
+        autoSelectInit();
+        controlStyleSelectInit();
+     
+		pdp = new PowerDistribution(RobotMap.PDPThreadPeriod);
+		pdp.startThread();
+		
+		tx1 = new TX1TurnON(RobotMap.PDPThreadPeriod);
+		tx1.startThread();
+		
+		dtIMU = new DrivetrainIMUGlobalPosition(RobotMap.PDPThreadPeriod);
+		dtIMU.startThread();
+		
+		
+        drivetrain.calibrateGyro();
+        driverstation = DriverStation.getInstance();
+        
+        SmartDashboard.putData("Autonomous Mode Chooser", Robot.autoChooser);
+        SmartDashboard.putData("Control Style Chooser", Robot.controlStyleChooser);
+		//ConsolePrinter.putData("Autonomous Mode Chooser", () -> {return Robot.autoChooser;}, true, false);
+		ConsolePrinter.putString("AutoName", () -> {return Robot.getAutoName();}, true, false);
+		ConsolePrinter.putString("Control Style Name", () -> {return Robot.getControlStyleName();}, true, false);
+		//ConsolePrinter.putBoolean("isPracticeBot", Robot.isPracticeRobot());
+		ConsolePrinter.putNumber("gameClock", () -> {return driverstation.getMatchTime();}, true, false);
+        ConsolePrinter.putNumber("Robot Pressure", () -> {return Robot.pneumatics.getPSI();}, true, false);
+        
+
+        
+        ConsolePrinter.putBoolean("Is Practice Bot", () -> {return isPracticeRobot();}, true, false);
+        
+        ConsolePrinter.startThread();
+        System.out.println("Robot Done Loading");
+		
+		
+		m_chooser.addDefault("Default Auto", kDefaultAuto);
+		m_chooser.addObject("My Auto", kCustomAuto);
+		SmartDashboard.putData("Auto choices", m_chooser);
+	}
+
+	/**
+	 * This autonomous (along with the chooser code above) shows how to select
+	 * between different autonomous modes using the dashboard. The sendable
+	 * chooser code works with the Java SmartDashboard. If you prefer the
+	 * LabVIEW Dashboard, remove all of the chooser code and uncomment the
+	 * getString line to get the auto name from the text box below the Gyro
+	 *
+	 * <p>You can add additional auto modes by adding additional comparisons to
+	 * the switch structure below with additional strings. If using the
+	 * SendableChooser make sure to add them to the chooser code above as well.
+	 */
+	@Override
+	public void autonomousInit() {
+		m_autoSelected = m_chooser.getSelected();
+		// m_autoSelected = SmartDashboard.getString("Auto Selector",
+		// 		kDefaultAuto);
+		System.out.println("Auto selected: " + m_autoSelected);
+	}
+
+	/**
+	 * This function is called periodically during autonomous.
+	 */
+	@Override
+	public void autonomousPeriodic() {
+		switch (m_autoSelected) {
+			case kCustomAuto:
+				// Put custom auto code here
+				break;
+			case kDefaultAuto:
+			default:
+				// Put default auto code here
+				break;
+		}
+	}
+
+	/**
+     * This method is called once each time the robot enters Disabled mode.
+     * You can use it to reset any subsystem information you want to clear when
+	 * the robot is disabled.
+     */
+    public void disabledInit(){
+    		autoMode = false;
+    		matchStarted = false;
+    }
+    
+	public void disabledPeriodic() {
+
+		SmartDashboard.putNumber("GunStyleYValueMakingThisLongSoWeCanFindIt", Robot.oi.driverJoystick.getLeftStickRaw_Y());
+        SmartDashboard.putNumber("GunStyleXValueMakingThisLongSoWeCanFindIt", Robot.oi.driverJoystick.getLeftStickRaw_X());
+        SmartDashboard.putNumber("GunStyleXInterpolatedValueMakingThisLongSoWeCanFindIt", Robot.drivetrain.getGunStyleXValue());
+		
+        getControlStyleInt();
+        controlStyle = (int) controlStyleChooser.getSelected();
+
+
+        autonomousCommand = (Command) autoChooser.getSelected();
+        
+		// Kill all active commands
+		Scheduler.getInstance().run();
+
+		
+		autoMode = false;
+		
+		// Check to see if the gyro is drifting, if it is re-initialize it.
+		gyroReinit();
+	}
+	
+    /**
+	 * Get the name of an autonomous mode command.
+	 * @return the name of the auto command.
+	 */
+	public static String getAutoName() {
+		if (autonomousCommand != null) {
+			return autonomousCommand.getName(); 
+		} else {
+			return "None";
+		}
+	}
+	
+    /**
+	 * Get the name of an control style.
+	 * @return the name of the control style.
+	 */
+	public static String getControlStyleName() {
+		String retVal = "";
+	
+		switch (controlStyle) {
+		case 0:
+			retVal = "Tank Drive";
+			break;
+		case 1:
+			retVal = "Gun Style";
+			break;
+		case 2:
+			retVal = "Arcade Drive";
+			break;
+		case 3:
+			retVal = "GTA Drive";
+			break;
+		default:
+			retVal = "Invalid Control Style";
+		}
+		
+		return retVal;
+	}
+    
+    /**
+     * Adds control styles to the selector
+     */
+    public void controlStyleSelectInit(){
+    	controlStyleChooser = new SendableChooser<>();    	
+    	controlStyleChooser.addObject("Tank Drive", 0);
+    	controlStyleChooser.addDefault("Gun Style Controller", 1);
+    	controlStyleChooser.addObject("Arcade Drive", 2);
+    	controlStyleChooser.addObject("GTA Drive", 3);
+    }
+    
+    public static int getControlStyleInt() {
+    	return (int) controlStyleChooser.getSelected();
+    }
+    
+
+    /**
+     * Adds the autos to the selector
+     */
+    public void autoSelectInit() {
+        autoChooser = new SendableChooser<Command>();
+        autoChooser.addObject("Do Nothing", new DoNothing());
+        //  autoChooser.addObject("Do Something", new DoSomething());
+    }
+    
+    /**
+	 *
+	 * @return true if the robot is in auto mode
+	 */
+	public static boolean isAutoMode() {
+		return autoMode;
+	}
+	
+
+    /**
+	 * Method which checks to see if gyro drifts and resets the gyro. Call this
+	 * in a loop.
+	 */
+	private void gyroReinit() {
+		//Check to see if the gyro is drifting, if it is re-initialize it.
+		//Thanks FRC254 for orig. idea.
+		curAngle = drivetrain.getHeading();
+		gyroCalibrating = drivetrain.isGyroCalibrating();
+
+		if (lastGyroCalibrating && !gyroCalibrating) {
+			//if we've just finished calibrating the gyro, reset
+			gyroDriftDetector.reset();
+			curAngle = drivetrain.getHeading();
+			System.out.println("Finished auto-reinit gyro");
+		} else if (gyroDriftDetector.update(Math.abs(curAngle - lastAngle) > (0.75 / 50.0))
+				&& !matchStarted && !gyroCalibrating) {
+			//&& gyroReinits < 3) {
+			gyroReinits++;
+			System.out.println("!!! Sensed drift, about to auto-reinit gyro ("+ gyroReinits + ")");
+			drivetrain.calibrateGyro();
+		}
+
+		lastAngle = curAngle;
+		lastGyroCalibrating = gyroCalibrating;
+	}
+
+	/**
+	 * Returns the status of DIO pin 24 
+	 *
+	 * @return true if this is the practice robot
+	 */
+	public static boolean isPracticeRobot() {
+		return !practiceBot.get();
+	}
+	
+	/**
+	 * Returns whether we are on blue (true or false)
+	 */
+	public static boolean onBlueAlliance(){
+		return driverstation.getAlliance() == DriverStation.Alliance.Blue;
+	}
+	
+	/**
+	 * This function is called periodically during operator control.
+	 */
+	@Override
+	public void teleopPeriodic() {
+	}
+
+	/**
+	 * This function is called periodically during test mode.
+	 */
+	@Override
+	public void testPeriodic() {
+	}
+}
---
+++ src/org/team2168/RobotMap.java
@@ -0,0 +1,260 @@
+
+package org.team2168;
+
+import org.team2168.PID.sensors.AverageEncoder;
+import edu.wpi.first.wpilibj.CounterBase;
+import edu.wpi.first.wpilibj.I2C;
+
+/**
+ * The RobotMap is a mapping from the ports sensors and actuators are wired into
+ * to a variable name. This provides flexibility changing wiring, makes checking
+ * the wiring easier and significantly reduces the number of magic numbers
+ * floating around.
+ */
+public class RobotMap {
+
+	public static final double MAIN_PERIOD_S = 0.005; //Main loop 200Hz
+	
+	/*************************************************************************
+	 *                              ROBORIO WIRING MAP
+	 *************************************************************************/
+
+	// Joysticks///////////////////////////////////////////////////////////////
+	public static final int DRIVER_JOYSTICK = 0;
+	public static final int OPERATOR_JOYSTICK = 1;
+	public static final int DRIVER_OPERATOR_E_BACKUP = 2;
+	public static final int COMMANDS_TEST_JOYSTICK = 4;
+	public static final int PID_TEST_JOYSTICK = 5;
+	
+	// Joystick Control Styles/////////////////////////////////////////////////
+	public static final int TANK_DRIVE_STYLE_ENUM = 0;
+	public static final int GUN_STYLE_ENUM = 1;
+	public static final int ARCADE_STYLE_ENUM = 2;
+	public static final int GTA_STYLE_ENUM = 3;
+	
+	// PWM (0 to 9) on RoboRio/////////////////////////////////////////////////
+	public static final int RIGHT_DRIVE_MOTOR_1 = 0;
+	public static final int RIGHT_DRIVE_MOTOR_2 = 1;
+	public static final int LEFT_DRIVE_MOTOR_1 = 2;
+	public static final int LEFT_DRIVE_MOTOR_2 = 3;	
+//	public static final int BALL_INTAKE_MOTOR = 4;
+//	public static final int CLIMBER_MOTOR_LEFT = 5;
+//	public static final int CLIMBER_MOTOR_RIGHT = 6;
+//	public static final int SHOOTER_WHEEL_LEFT = 7;
+//	public static final int SHOOTER_WHEEL_RIGHT = 8;
+//
+//	public static final int SHOOTER_HOOD_SERVO = 9;
+	// More PWM channels in common PWM/DIO MXP section below.
+
+
+	//Digital IO Channels//////////////////////////////////////////////////////
+	//Channels 0-9 on RoboRio
+	public static final int RIGHT_DRIVE_ENCODER_A = 0;
+	public static final int RIGHT_DRIVE_ENCODER_B = 1;	
+//	public static final int BALL_INTAKE_ARM_HALL_EFFECT = 2;
+//	public static final int GEAR_INTAKE_ARM_HALL_EFECT = 3;
+	public static final int LEFT_DRIVE_ENCODER_B = 4;
+	public static final int LEFT_DRIVE_ENCODER_A = 5;
+//	public static final int INDEXER_LOWER_BALL_PRESENT = 6;
+//	public static final int INDEXER_UPPER_BALL_PRESENT = 7;
+	public static final int TX1_TURN_ON = 8;
+	public static final int TX1_ON_STATUS = 9;
+
+
+	//Channels 10-25 on MXP (PWM and DIO)
+//	public static final int CONVELATOR_MOTOR = 10;
+//	public static final int TURRET_MOTOR = 11;
+//	public static final int GEAR_INTAKE_MOTOR = 12;
+//	public static final int INDEXER_WHEEL = 13;
+//	public static final int AGITATOR_WHEEL = 14;
+//	public static final int TURRET_LIMIT_SWITCH_RIGHT = 19; //PWM 15 on board
+//	public static final int TURRET_LIMIT_SWITCH_LEFT = 20; //PWM 16 on board
+//	public static final int SHOOTER_ENCODER_A = 21; //PWM 17 on board
+//	public static final int SHOOTER_ENCODER_B = 22; //PWM 18 on board
+	public static final int PRACTICE_BOT_JUMPER = 24;
+	
+	//PBOT Differences
+//	public static final int GEAR_INTAKE_ARM_HALL_EFECT_PBOT = 0;
+	public static final int LEFT_DRIVE_ENCODER_B_PBOT = 1;
+	public static final int LEFT_DRIVE_ENCODER_A_PBOT = 2;
+	public static final int RIGHT_DRIVE_ENCODER_A_PBOT = 3;
+	public static final int RIGHT_DRIVE_ENCODER_B_PBOT = 4;
+//	public static final int BALL_INTAKE_ARM_HALL_EFFECT_PBOT = 5;
+//	public static final int SHOOTER_ENCODER_A_PBOT = 6; 
+//	public static final int SHOOTER_ENCODER_B_PBOT = 7;
+//	public static final int INDEXER_LOWER_BALL_PRESENT_PBOT = 21; //PWM 17 on board
+//	public static final int INDEXER_UPPER_BALL_PRESENT_PBOT = 22; //PWM 18 on board
+	
+	
+	//Solenoid Channels////////////////////////////////////////////////////////
+	public final static int DRIVETRAIN_HIGH_GEAR = 0;
+	public final static int DRIVETRAIN_LOW_GEAR= 1;
+//	public final static int GEAR_INTAKE_PISTON_EXTEND = 2;
+//	public final static int GEAR_INTAKE_PISTON_RETRACT = 3;
+//	public final static int BALL_INTAKE_PISTON_EXTEND = 4;
+//	public final static int BALL_INTAKE_PISTON_RETRACT = 5;
+
+	//Analog Input Channels////////////////////////////////////////////////////
+	//Channels 0-3 on Roborio
+//	public static final int TURRET_POTENTIOMETER = 0;
+//	public static final int GEAR_INTAKE_ROLLER_IR = 1;
+	public static final int DRIVETRAIN_IR_SENSOR = 2;
+	public static final int PRESSURE_SENSOR = 3;
+
+
+	//Channels 4-7 on MXP
+
+
+	//TODO: Confirm PDP Ports
+	//TODO: Should be changed to match the new configuration 
+	//PDP Channels/////////////////////////////////////////////////////////////
+	public final static int DRIVETRAIN_RIGHT_MOTOR_1_PDP = 12;
+	public final static int DRIVETRAIN_RIGHT_MOTOR_2_PDP = 3;
+	public static final int BALL_INTAKE_MOTOR_PDP = 11;
+	public static final int TURRET_MOTOR_PDP = 5;
+	public static final int CLIMBER_MOTOR_LEFT_PDP = 13;
+	public static final int CLIMBER_MOTOR_RIGHT_PDP = 14;
+	public static final int ELEVATOR_MOTOR_PDP = 4;
+	public final static int GEAR_INTAKE_MOTOR_PDP = 6;
+	public final static int DRIVETRAIN_LEFT_MOTOR_1_PDP = 1;
+	public final static int DRIVETRAIN_LEFT_MOTOR_2_PDP = 2;
+	
+	public static final int INDEXER_WHEEL_PDP = 10;
+	public final static int SHOOTER_MOTOR_LEFT_PDP = 0;
+	public final static int SHOOTER_MOTOR_RIGHT_PDP = 15;
+	public final static int PCM_POWER = 7;;
+	
+	//CAN Device IDs///////////////////////////////////////////////////////////
+
+
+	//Relay Channels///////////////////////////////////////////////////////////
+	public static final int FLASHLIGHT_RELAY = 0;
+
+
+	/*************************************************************************
+	 *                         DRIVETRAIN PARAMETERS
+	 *************************************************************************/
+	//TODO check if the reverse values match the physical robot
+	public static final boolean DT_REVERSE_RIGHT = true;
+	public static final boolean DT_REVERSE_LEFT = false;
+
+	private static final int DRIVE_PULSE_PER_ROTATION = 256; //encoder ticks per rotation
+	//TODO find ratio
+	private static final double DRIVE_GEAR_RATIO = 1.0/1.0; //ratio between wheel over encoder
+	private static final double DRIVE_WHEEL_DIAMETER = 6;
+	public static final int DRIVE_ENCODER_PULSE_PER_ROT = (int) (DRIVE_PULSE_PER_ROTATION * DRIVE_GEAR_RATIO); //pulse per rotation * gear ratio
+	public static final double DRIVE_ENCODER_DIST_PER_TICK = (Math.PI * DRIVE_WHEEL_DIAMETER / DRIVE_ENCODER_PULSE_PER_ROT);
+	public static final CounterBase.EncodingType DRIVE_ENCODING_TYPE = CounterBase.EncodingType.k4X; //count rising and falling edges on
+	public static final AverageEncoder.PositionReturnType DRIVE_POS_RETURN_TYPE = AverageEncoder.PositionReturnType.FEET;
+	public static final AverageEncoder.SpeedReturnType DRIVE_SPEED_RETURN_TYPE = AverageEncoder.SpeedReturnType.FPS;
+	public static final int DRIVE_ENCODER_MIN_RATE = 0;
+	public static final int DRIVE_ENCODER_MIN_PERIOD = 1;
+	public static final boolean LEFT_DRIVE_TRAIN_ENCODER_REVERSE = true;
+	public static final boolean RIGHT_DRIVE_TRAIN_ENCODER_REVERSE = true;
+	public static final boolean LEFT_DRIVE_TRAIN_ENCODER_REVERSE_PBOT = true;
+	public static final boolean RIGHT_DRIVE_TRAIN_ENCODER_REVERSE_PBOT = true;
+	public static final int DRIVE_AVG_ENCODER_VAL = 5;
+	public static final double MIN_DRIVE_SPEED = 0.2;
+	public static final double AUTO_NORMAL_SPEED = 0.5;
+	public static final double WHEEL_BASE = 2; //units must match PositionReturnType (feet)
+	/*************************************************************************
+	 *                            PDP PARAMETERS
+	 *************************************************************************/
+	public final static long PDPThreadPeriod = 20;
+	public static final double WARNING_CURRENT_LIMIT = 20;
+	public static final double STALL_CURRENT_LIMIT = 35;
+	public static final double CURRENT_LIMIT_TIME_THRESHOLD_SECONDS = 1; 
+
+	/*************************************************************************
+	 *                            PID PARAMETERS
+	 *************************************************************************/
+	//period to run PID loops on drive train
+	public static final long DRIVE_TRAIN_PID_PERIOD = 20;//70ms loop
+	public static final int DRIVE_TRAIN_PID_ARRAY_SIZE = 30;
+
+	//PID Gains for Left & Right Speed and Position
+	//Bandwidth =
+	//Phase Margin =
+	public static final double DRIVE_TRAIN_LEFT_SPEED_P =  0.4779;
+	public static final double DRIVE_TRAIN_LEFT_SPEED_I =  1.0526;
+	public static final double DRIVE_TRAIN_LEFT_SPEED_D =  0.0543;
+
+	public static final double DRIVE_TRAIN_RIGHT_SPEED_P = 0.4779;
+	public static final double DRIVE_TRAIN_RIGHT_SPEED_I = 1.0526;
+	public static final double DRIVE_TRAIN_RIGHT_SPEED_D = 0.0543;
+
+	public static final double DRIVE_TRAIN_LEFT_POSITION_P = 0.2;
+	public static final double DRIVE_TRAIN_LEFT_POSITION_I = 0.0001412646174233;
+	public static final double DRIVE_TRAIN_LEFT_POSITION_D = 0.0074778888124088;
+
+	public static final double DRIVE_TRAIN_RIGHT_POSITION_P = 0.25;
+	public static final double DRIVE_TRAIN_RIGHT_POSITION_I = 0.0001412646174233;
+	public static final double DRIVE_TRAIN_RIGHT_POSITION_D = 0.0074778888124088;
+
+	public static final double ROTATE_POSITION_P = 0.024;
+	public static final double ROTATE_POSITION_I = 0.027;
+	public static final double ROTATE_POSITION_D = 0.000000067;
+	
+	
+	public static final double ROTATE_POSITION_P_CAM = 0.024;
+	public static final double ROTATE_POSITION_I_CAM = 0.027;
+	public static final double ROTATE_POSITION_D_CAM = 0.000000067;
+	
+
+	public static final double ROTATE_POSITION_CAMERA_MAX = 0.28;
+	public static final double ROTATE_POSITION_CAMERA_MIN = 0.15;
+	
+	public static final double ROTATE_POSITION_P_Drive_Straight = 0.045;
+	public static final double ROTATE_POSITION_I_Drive_Straight = 0.001;
+	public static final double ROTATE_POSITION_D_Drive_Straight = 0.0064778888124088;
+	
+	public static final double ROTATE_TURRET_P = 0.024;
+	public static final double ROTATE_TURRET_I = 0.027;
+	public static final double ROTATE_TURRET_D = 0.000000067;
+
+	//Shooter PID Speed
+	//Bandwidth =
+	//Phase Margin =
+//	public static final double SHOOTER_SPEED_P = 0.000035;
+//	public static final double SHOOTER_SPEED_I = 0.000053; 
+//	public static final double SHOOTER_SPEED_D = 0.0000011838;
+//	public static final double SHOOTER_SPEED_N = 6.8807;
+	
+	public static final double SHOOTER_SPEED_P = 0.0000727;
+	public static final double SHOOTER_SPEED_I = 0.000093; 
+	public static final double SHOOTER_SPEED_D = 0.0000015838;
+	public static final double SHOOTER_SPEED_N = 100;
+	
+	
+	//Turret Period
+	public static final long TURRET_PID_PERIOD = 20;
+	public static final int TURRET_PID_ARRAY_SIZE = 30;
+		
+
+	/****************************************************************
+	 *                TCP Servers  (ONLY FOR DEBUGGING)             *
+	 ****************************************************************/
+	public static final int TCP_SERVER_DRIVE_TRAIN_POS = 1180;
+	public static final int TCP_SERVER_ROTATE_CONTROLLER = 1181;
+	public static final int TCO_SERVER_RIGHT_DRIVE_TRAIN_SPEED = 1182;
+	public static final int TCP_SERVER_LEFT_DRIVE_TRAIN_SPEED = 1183;
+	public static final int TCP_SERVER_SHOOTER_SPEED = 1184;
+	public static final int TCP_SERVER_ROTATE_CONTROLLER_STRAIGHT = 1185;
+	public static final int TCP_SERVER_ROTATE_CAMERA_CONTROLLER = 1186;
+	public static final int TCP_SERVER_ROTATE_TURRET_CAMERA_CONTROLLER = 1187;
+	public static final int TCP_SERVER_ROTATE_TURRET_POT_CONTROLLER = 1188;
+
+	/******************************************************************
+	 *                    ConsolePrinter PARAMETERS                   *
+	 ******************************************************************/
+	public static final boolean PRINT_SD_DEBUG_DATA = true;
+	public static final long SmartDashThreadPeriod = 100; //ms
+	public static final long CONSOLE_PRINTER_LOG_RATE_MS = 100; //ms
+
+	
+	/******************************************************************
+	 *                         Lights I2C                             *
+	 ******************************************************************/
+	public static final I2C.Port I2C_PORT = I2C.Port.kOnboard;
+	public static final int I2C_ADDRESS = 10;
+}
