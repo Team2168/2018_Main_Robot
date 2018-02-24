cc628c7cdf771f45cc46700b0e96d5aa74d323c1
David Martinez <davidm2556@gmail.com>
2018-02-24 17:38:15 -0500

    Fixed pivot motor subsystem

--- src/org/team2168/subsystems/CubeIntakePivot.java
+++ src/org/team2168/subsystems/CubeIntakePivot.java
@@ -40,7 +40,7 @@ public class CubeIntakePivot extends Subsystem {
 		
 		ConsolePrinter.putBoolean("Is Intake Pivot Fully Up", () -> {return isRaised();}, true, false);
 		ConsolePrinter.putBoolean("Is Intake Pivot Fully Down", () -> {return isLowered();}, true, false);
-		ConsolePrinter.putNumber("Pivot motor voltage", () -> {return getMotorVoltage();}, true, true);
+		ConsolePrinter.putNumber("Pivot motor voltage", () -> {return getMotorVoltage();}, true, false);
 		ConsolePrinter.putNumber("Is fully up int", () -> {return (double)getIsFullyRaisedInt();}, true, false);
 		ConsolePrinter.putNumber("Is fully down int", () -> {return (double)getIsFullyLoweredInt();}, true, false);
 		ConsolePrinter.putNumber("Pivot Motor current ", () -> {return Robot.pdp.getChannelCurrent(RobotMap.INTAKE_PIVOT_MOTOR_PDP);}, true, false);	
