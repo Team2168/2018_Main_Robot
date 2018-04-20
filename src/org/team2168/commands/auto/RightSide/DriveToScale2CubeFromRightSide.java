7a4917b7a1ca054b8f3b74ecdde37eb42440064c
David Martinez <davidm2556@gmail.com>
2018-04-19 21:54:06 -0400

    Fixed turn logic

--- src/org/team2168/commands/auto/RightSide/DriveToScale2CubeFromRightSide.java
+++ src/org/team2168/commands/auto/RightSide/DriveToScale2CubeFromRightSide.java
@@ -40,7 +40,7 @@ public class DriveToScale2CubeFromRightSide extends CommandGroup {
     	addSequential(new DrivePIDPath(2.8,true)); //drive back 2.2
     	addSequential(new CloseIntake());
     	addSequential(new DriveLiftPIDZZZ(45, 0.7, 0.2,1.0,true)); //drive lift down slowly
-    	addParallel(new  DrivePIDPathQuintic(-20, 210, 2500, 3000, 30000));//rotate A to B
+    	addParallel(new  DrivePIDPathQuintic(340, 210, 2500, 3000, 30000));//rotate A to B
     	addSequential(new DriveLiftPIDZZZ(0.5, 0.7, 0.2,1.0,true)); //drive lift down slowly
     	addSequential(new RotateXDistancePIDZZZ(210,0.5,0.15,0.1,true),0.2);
     	
@@ -58,8 +58,8 @@ public class DriveToScale2CubeFromRightSide extends CommandGroup {
     	//addParallel(new ExtendPivotWithPiston());
     	addSequential(new DrivePIDPath(1.5,true)); //drive back 2.5
     	
-    	addSequential(new  DrivePIDPathQuintic(210, -20, 2500, 3000, 30000));//rotate A to B
-    	addParallel(new RotateXDistancePIDZZZ(-20.0,0.9,0.2,0.1,true));
+    	addSequential(new  DrivePIDPathQuintic(210, 340, 2500, 3000, 30000));//rotate A to B
+    	addParallel(new RotateXDistancePIDZZZ(340.0,0.9,0.2,0.1,true));
     	addSequential(new DriveLiftPIDZZZ(80.0, 1.0, 0.1,1.0,true)); //70
     	addSequential(new DrivePIDPath(1.5)); //drive back 2.2
     	addSequential(new DriveIntakeWheelsWithConstant(-0.3), 0.3);
@@ -69,7 +69,7 @@ public class DriveToScale2CubeFromRightSide extends CommandGroup {
     	addSequential(new DrivePIDPath(2.8,true)); //drive back 2.2
     	addParallel(new CloseIntake());
     	addSequential(new DriveLiftPIDZZZ(45, 0.7, 0.2,1.0,true)); //drive lift down slowly
-    	addParallel(new  DrivePIDPathQuintic(-20, 120, 2500, 3000, 30000));//rotate A to B
+    	addParallel(new  DrivePIDPathQuintic(340, 120, 2500, 3000, 30000));//rotate A to B
     	addSequential(new DriveLiftPIDZZZ(0.5, 0.7, 0.2,1.0,true)); //drive lift down slowly
     	addSequential(new RotateXDistancePIDZZZ(240,0.5,0.15,0.1,true),0.2);
     	
